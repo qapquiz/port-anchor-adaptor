8db4dba2c0a09f0d42ce6359ed3163fd434992a1
steve <dhz@port.finance>
2021-11-29 15:40:12 +0800

    init

--- src/error.rs
+++ src/error.rs
@@ -1,27 +1,13 @@
-use num_derive::FromPrimitive;
-use anchor_lang::solana_program::{decode_error::DecodeError, program_error::ProgramError};
-use thiserror::Error;
+use anchor_lang::prelude::*;
 
-#[derive(Clone, Debug, Eq, Error, FromPrimitive, PartialEq)]
+#[error]
 pub enum PortAdaptorError {
-    #[error("Only has borrows, no collaterals")]
+    #[msg("Only has borrows, no collaterals")]
     Insolvency,
 
-    #[error("CollateralIndexOutOfBound")]
+    #[msg("CollateralIndexOutOfBound")]
     CollateralIndexOutOfBound,
-    #[error("BorrowIndexOutOfBound")]
+    #[msg("BorrowIndexOutOfBound")]
     BorrowIndexOutOfBound
 
-}
-
-impl From<PortAdaptorError> for ProgramError {
-    fn from(e: PortAdaptorError) -> Self {
-        ProgramError::Custom(e as u32)
-    }
-}
-
-impl<T> DecodeError<T> for PortAdaptorError {
-    fn type_of() -> &'static str {
-        "PortAdaptor Error"
-    }
 }
