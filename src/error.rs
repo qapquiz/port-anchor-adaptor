1c4d8ea7ff8b625bc050c741676f188acd81820e
steve <dhz@port.finance>
2021-11-29 15:40:19 +0800

    init

--- src/error.rs
+++ src/error.rs
@@ -4,10 +4,8 @@ use anchor_lang::prelude::*;
 pub enum PortAdaptorError {
     #[msg("Only has borrows, no collaterals")]
     Insolvency,
-
     #[msg("CollateralIndexOutOfBound")]
     CollateralIndexOutOfBound,
     #[msg("BorrowIndexOutOfBound")]
     BorrowIndexOutOfBound
-
 }
